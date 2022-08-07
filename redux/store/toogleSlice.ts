cdddc0c63c0d8b1402d161a1d800da224201ff09
DehLuizSantos <and_consul@outlook.com>
2022-08-07 16:54:01 -0300

    fix(deploy):hidratation-error-redux

--- redux/store/toogleSlice.ts
+++ redux/store/toogleSlice.ts
@@ -21,17 +21,6 @@ export const toogleSlice = createSlice({
     setToogle(state, action) {
       state.toogleState = action.payload;
     },
-
-    // Special reducer for hydrating the state. Special case for next-redux-wrapper
-    extraReducers: {
-      [HYDRATE]: (state, action) => {
-        return {
-        ...state,
-        ...action.payload.auth,
-        };
-      },
-    },
-
   },
 });
 
